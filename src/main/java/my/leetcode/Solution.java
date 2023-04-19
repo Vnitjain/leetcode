0d0726a24dea896588f7978395ead7feb9239b79
Vinit Jain <jain.vinit14@gmail.com>
2023-04-19 21:37:45 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,16 @@
 package my.leetcode;
 
+import java.util.Arrays;
+
 public class Solution {
-    
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 3; i >= 0; --i) {
+            int s1 = nums[i], s2 = nums[i + 1], s3 = nums[i + 2];
+            if (s1 + s2 > s3) {
+                return s1 + s2 + s3;
+            }
+        }
+        return 0;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,12 +1,22 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testCases() {
+        assertEquals(null, 5, s.largestPerimeter(new int[] { 2, 1, 2 }), 0);
+        assertEquals(null, 0, s.largestPerimeter(new int[] { 1, 2, 1, 10 }), 0);
+    }
 
+    @Test
+    public void failedTestCases() {
+        assertEquals(null, 8, s.largestPerimeter(new int[] { 3, 6, 2, 3 }), 0);
+        assertEquals(null, 5, s.largestPerimeter(new int[] { 2, 1, 2 }), 0);
+        assertEquals(null, 10, s.largestPerimeter(new int[] { 3, 2, 3, 4 }), 0);
     }
 }
