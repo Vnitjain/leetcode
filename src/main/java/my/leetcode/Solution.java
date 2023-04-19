351cc620a6b6da8154dc92b3e8eee4b086d5953c
Vinit Jain <jain.vinit14@gmail.com>
2023-04-19 17:04:58 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,12 @@
 package my.leetcode;
 
+//https://leetcode.com/problems/remove-palindromic-subsequences/
 public class Solution {
-    
+    public int removePalindromeSub(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j)
+            if (s.charAt(i++) != s.charAt(j--))
+                return 2;
+        return 1;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,10 +1,22 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
-    @Test
-    public void shouldAnswerWithTrue() {
+    Solution s = new Solution();
 
+    @Test
+    public void solutionExampleTestCases() {
+        assertEquals(null, 1, s.removePalindromeSub("ababa"), 0);
+        assertEquals(null, 2, s.removePalindromeSub("abb"), 0);
+        assertEquals(null, 2, s.removePalindromeSub("baabb"), 0);
+
+    }
+
+    @Test
+    public void wrongTestCases() {
+        assertEquals(null, 2, s.removePalindromeSub("bbaabaaa"), 0);
     }
 }
