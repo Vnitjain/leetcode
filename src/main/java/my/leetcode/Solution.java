1e33e4697a4d407a2a9725be1feacb84b307355b
Vinit Jain <jain.vinit14@gmail.com>
2023-04-23 20:11:42 +0530

    not resolved yet

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,25 @@
 package my.leetcode;
 
-public class Solution {
-    
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+
+//https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+class Solution {
+    public boolean equalFrequency(String word) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char i : word.toCharArray()) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        System.out.println(map.toString());
+        boolean oneUsed = false;
+        int initialElement = -1;
+        for (int i : map.values()) {
+            if (initialElement == -1) {
+                initialElement = i;
+            }
+
+        }
+        return true;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,12 +1,22 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testCases() {
+        assertTrue(s.equalFrequency("abcc"));
+        assertFalse(s.equalFrequency("aazz"));
+    }
 
+    @Test
+    public void failedTestCases() {
+        assertTrue(s.equalFrequency("bac"));
+        assertTrue(s.equalFrequency("ddaccb"));
     }
 }
