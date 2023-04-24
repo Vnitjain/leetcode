0741801ad0cade02229275bc0015dbb8a177c669
Vinit Jain <jain.vinit14@gmail.com>
2023-04-25 01:58:18 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,21 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public boolean digitCount(String num) {
+        int[] count = new int[num.length()];
+        for (int i = 0; i < num.length(); i++) {
+            int current = Character.digit(num.charAt(i), 10);
+            if (current < num.length()) {
+                count[current]++;
+            } else {
+                return false;
+            }
+        }
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] != Character.digit(num.charAt(i), 10)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,12 +1,16 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
 
     @Test
-    public void shouldAnswerWithTrue() {
-
+    public void testDigitCount() {
+        assertTrue(s.digitCount("1210"));
+        assertFalse(s.digitCount("030"));
     }
 }
