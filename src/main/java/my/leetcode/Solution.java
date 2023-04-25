04c2cbf36a8846f0f7bbf10661444ae769884ee3
Vinit Jain <jain.vinit14@gmail.com>
2023-04-25 15:54:00 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,23 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public boolean checkZeroOnes(String s) {
+        int[] max = new int[2], active = new int[] { s.charAt(0) - 48, 1 };
+        for (int i = 1; i < s.length(); i++) {
+            int currentInt = s.charAt(i) - 48;
+            if (currentInt == active[0]) {
+                ++active[1];
+            } else {
+                if (max[active[0]] < active[1]) {
+                    max[active[0]] = active[1];
+                }
+                active[0] = currentInt;
+                active[1] = 1;
+            }
+        }
+        if (active[1] > max[active[0]]) {
+            max[active[0]] = active[1];
+        }
+        return max[1] > max[0];
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,12 +1,27 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testCheckZeroOnes() {
+        assertTrue(s.checkZeroOnes("1101"));
+        assertFalse(s.checkZeroOnes("111000"));
+        assertFalse(s.checkZeroOnes("110100010"));
+    }
 
+    @Test
+    public void failedTestCheckZeroOnes() {
+        assertFalse(s.checkZeroOnes("110100010"));
+    }
+
+    @Test
+    public void tp() {
+        System.out.println((int) '0');
     }
 }
