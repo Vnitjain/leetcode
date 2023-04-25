8a23563d09e5410618a22b1289ae5d720d826dd8
Vinit Jain <jain.vinit14@gmail.com>
2023-04-25 13:24:38 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,27 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1)
+            return true;
+        if (isCapital(word.charAt(0))) {
+            if (isCapital(word.charAt(1))) {
+                for (int i = 2; i < word.length(); i++)
+                    if (!isCapital(word.charAt(i)))
+                        return false;
+            } else {
+                for (int i = 2; i < word.length(); i++)
+                    if (isCapital(word.charAt(i)))
+                        return false;
+            }
+        } else
+            for (int i = 1; i < word.length(); i++)
+                if (isCapital(word.charAt(i)))
+                    return false;
+        return true;
+    }
+
+    public boolean isCapital(char a) {
+        return a <= 'z' && a >= 'a' ? false : true;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,12 +1,24 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testCases() {
+        assertTrue(s.detectCapitalUse("USA"));
+        assertFalse(s.detectCapitalUse("FlaG"));
+    }
 
+    @Test
+    public void testIsCapitalMethod() {
+        assertTrue(s.isCapital('Z'));
+        assertFalse(s.isCapital('z'));
+        assertTrue(s.isCapital('A'));
+        assertFalse(s.isCapital('a'));
     }
 }
