7cd74a6f47a1f721495230ea10fb26d6e2d717aa
Vinit Jain <jain.vinit14@gmail.com>
2023-05-03 21:30:20 +0530

    added millisecond timer in apptest file

--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -4,9 +4,18 @@ import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
+    long startTime;
 
     @Test
     public void shouldAnswerWithTrue() {
 
+    }
+
+    public void startTimer() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTimer() {
+        System.out.println(System.currentTimeMillis() - startTime);
     }
 }
