e818e59cb8ce347403343e8089545210bf571a3a
Vinit Jain <jain.vinit14@gmail.com>
2023-05-04 00:36:13 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,13 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public int[] divisibilityArray(String word, int m) {
+        int[] finalResult = new int[word.length()];
+        long remainder = 0;
+        for (int i = 0; i < word.length(); i++) {
+            remainder = ((remainder * 10) + (word.charAt(i) - '0')) % m;
+            finalResult[i] = remainder == 0 ? 1 : 0;
+        }
+        return finalResult;
+    }
 }
