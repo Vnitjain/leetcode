4fee25438f044068d893b7f549d566ebbb9199a4
Vinit Jain <jain.vinit14@gmail.com>
2023-05-03 21:22:12 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,28 @@
 package my.leetcode;
 
-public class Solution {
-    
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>> finalList = new HashSet<>();
+        List<Integer> tempList = new ArrayList<>();
+        traverse(0, tempList, finalList, Integer.MIN_VALUE, nums);
+        return new ArrayList<>(finalList);
+    }
+
+    private void traverse(int currentIndex, List<Integer> tempList, Set<List<Integer>> finalList, int prev,
+            int[] nums) {
+        if (tempList.size() > 1)
+            finalList.add(new ArrayList<>(tempList));
+        for (int i = currentIndex; i < nums.length; i++) {
+            if (nums[i] >= prev) {
+                tempList.add(nums[i]);
+                traverse(i + 1, tempList, finalList, nums[i], nums);
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -4,9 +4,34 @@ import org.junit.Test;
 
 public class AppTest {
     Solution s = new Solution();
+    long startTime;
 
     @Test
     public void shouldAnswerWithTrue() {
 
     }
+
+    @Test
+    public void tp() {
+        startTimer();
+        // s.findSubsequences(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+        // 15 });
+        s.findSubsequences(new int[] { 4, 6, 7, 7 });
+        // s.findSubsequences(new int[] { 1, 2, 3 });
+        // s.traverse(0, new int[] { 1, 2, 3 });
+        stopTimer();
+    }
+
+    public void startTimer() {
+        startTime = System.currentTimeMillis();
+    }
+
+    public void stopTimer() {
+        System.out.println(System.currentTimeMillis() - startTime);
+    }
+    /*
+     * 
+     * Input: nums = [4,6,7,7]
+     * Output: [[4,6],[4,6,7],[4,6,7,7],[4,7],[4,7,7],[6,7],[6,7,7],[7,7]]
+     */
 }
