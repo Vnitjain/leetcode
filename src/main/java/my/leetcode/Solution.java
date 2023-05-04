826939ae01bb2dc46d0d378526f64b87aae2e8c6
Vinit Jain <jain.vinit14@gmail.com>
2023-05-05 02:35:54 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,20 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int needleLength = needle.length();
+        for (int i = 0; i < haystack.length() - needleLength + 1; i++) {
+            int j = 0;
+            while (j < needleLength) {
+                if (needle.charAt(j) != haystack.charAt(j + i)) {
+                    break;
+                }
+                j++;
+            }
+            if (j == needleLength) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,7 +10,25 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        startTimer();
+        assertEquals(0, s.strStr("sadbutsad", "sad"));
+        stopTimer();
 
+        startTimer();
+        assertEquals(-1, s.strStr("leetcode", "leeto"));
+        stopTimer();
+
+        startTimer();
+        assertEquals(2, s.strStr("leetcode", "etc"));
+        stopTimer();
+
+        startTimer();
+        assertEquals(-1, s.strStr("mississippi", "issipi"));
+        stopTimer();
+
+        startTimer();
+        assertEquals(0, s.strStr("a", "a"));
+        stopTimer();
     }
 
     private void startTimer() {
