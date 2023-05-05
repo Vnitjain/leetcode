3984bcf25dec79f30b45e307959f2b0a5646d6a3
Vinit Jain <jain.vinit14@gmail.com>
2023-05-05 15:46:33 +0530

    another solution

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,24 +1,19 @@
 package my.leetcode;
 
 class Solution {
-    public boolean repeatedSubstringPattern(String s) {
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (doesItRepeatInString(s.substring(0, i + 1), s))
-                return true;
+    public boolean repeatedSubstringPattern(String str) {
+        int stringLength = str.length();
+        for (int i = stringLength / 2; i >= 1; i--) {
+            if (stringLength % i == 0) {
+                int count = stringLength / i;
+                String subString = str.substring(0, i);
+                String tempString = "";
+                for (int j = 0; j < count; j++)
+                    tempString += subString;
+                if (tempString.equals(str))
+                    return true;
+            }
         }
         return false;
-    }
-
-    private boolean doesItRepeatInString(String searchTerm, String s) {
-        int searchTermLength = searchTerm.length();
-        int i = searchTermLength;
-        if (s.length() % searchTermLength != 0)
-            return false;
-        while (i < s.length()) {
-            if (!s.substring(i, i + searchTermLength).equals(searchTerm))
-                return false;
-            i += searchTermLength;
-        }
-        return true;
     }
 }
