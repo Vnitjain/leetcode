057c73992f99bc46c4c9c7930910101062b973c8
Vinit <jain.vinit14@gmail.com>
2023-05-05 16:48:09 +0530

    initial solution

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int j = i;
+                while (++j < nums.length) {
+                    if (nums[j] != 0) {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,10 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,7 +13,17 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        startTimer();
+        int[] nums = new int[] { 0, 1, 0, 3, 12 };
+        s.moveZeroes(nums);
+        assertArrayEquals(new int[] { 1, 3, 12, 0, 0 }, nums);
+        stopTimer();
 
+        startTimer();
+        nums = new int[] { 0 };
+        s.moveZeroes(nums);
+        assertArrayEquals(new int[] { 0 }, nums);
+        stopTimer();
     }
 
     private void startTimer() {
