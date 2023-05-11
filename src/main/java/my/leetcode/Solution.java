3493e805e2ac032d5e93d51a360ace31188afeae
Vinit <jain.vinit14@gmail.com>
2023-05-12 01:12:52 +0530

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,9 +1,16 @@
 package my.leetcode;
 
+import java.util.Arrays;
+
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        for (int i : arr) {
-
-        }
+        if (arr.length < 3)
+            return true;
+        Arrays.sort(arr);
+        int difference = Math.abs(arr[0] - arr[1]);
+        for (int i = 1; i < arr.length - 1; i++)
+            if (Math.abs(arr[i] - arr[i + 1]) != difference)
+                return false;
+        return true;
     }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,6 +10,17 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        startTimer();
+        assertEquals(true, s.canMakeArithmeticProgression(new int[] { 3, 5, 1 }));
+        stopTimer();
+
+        startTimer();
+        assertEquals(false, s.canMakeArithmeticProgression(new int[] { 1, 2, 4 }));
+        stopTimer();
+    }
+
+    @Test
+    public void tp() {
 
     }
 
