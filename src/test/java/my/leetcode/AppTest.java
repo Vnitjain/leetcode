8a7886b40f180c13de885288444471ee0cd79bb5
Vinit <jain.vinit14@gmail.com>
2023-07-03 20:57:25 +0530

    resolved

--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,6 +10,17 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        startTimer();
+        assertEquals(1, s.isWinner(new int[] { 4, 10, 7, 9 }, new int[] { 6, 5, 2, 3 }));
+        stopTimer();
+
+        startTimer();
+        assertEquals(2, s.isWinner(new int[] { 3, 5, 7, 6 }, new int[] { 8, 10, 10, 2 }));
+        stopTimer();
+
+        startTimer();
+        assertEquals(0, s.isWinner(new int[] { 2, 3 }, new int[] { 4, 1 }));
+        stopTimer();
 
     }
 
