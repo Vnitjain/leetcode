5642f4e1002d32bb2d985511b0cd2e298d1dac12
Vinit <jain.vinit14@gmail.com>
2023-07-05 14:15:35 +0530

    resolve

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,35 @@
 package my.leetcode;
 
-public class Solution {
-    
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode current = head.next, prev = head;
+        while (current != null) {
+            if (current.val == prev.val) {
+                prev.next = current.next;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+        return head;
+    }
 }
