352770fb6aeb7275f16c0f8c54d02d2563e3ea1b
Vinit <jain.vinit14@gmail.com>
2023-12-16 13:21:02 -0500

    temp commit

--- src/main/java/my/leetcode/App.java
+++ src/main/java/my/leetcode/App.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import java.io.FileNotFoundException;
+
 public class App {
     public static void main(String[] args) {
 
--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,11 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public int mySqrt(int x) {
+        for (int i = 0; i < x / 2; i++) {
+
+        }
+        return 0;
+    }
+
 }
