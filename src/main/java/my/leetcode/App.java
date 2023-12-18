30aec10d6f6e9dae55bf13582a1cfa8d09a062c6
Vinit <jain.vinit14@gmail.com>
2023-12-18 00:34:58 -0500

    solved

--- src/main/java/my/leetcode/App.java
+++ src/main/java/my/leetcode/App.java
@@ -2,6 +2,9 @@ package my.leetcode;
 
 public class App {
     public static void main(String[] args) {
+        Solution s = new Solution();
 
+        // System.out.println(s.countSegments(" "));
+        System.out.println(s.countSegments("Hello, my name is John"));
     }
 }
--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,15 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public int countSegments(String s) {
+        if (s.length() == 0)
+            return 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (s.length() == i + 1 || s.charAt(i + 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
