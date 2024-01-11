7ec2bf379bb6d6981fca76054828a640916f3a85
Vinit <jain.vinit14@gmail.com>
2024-01-11 14:38:11 -0500

    Solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int p1 = 0, p2 = height.length - 1;
+        while (p1 < p2) {
+            int left = height[p1];
+            int right = height[p2];
+            maxArea = Math.max(maxArea, Math.min(left, right) * (p2 - p1));
+            if (left > right)
+                p2--;
+            else
+                p1++;
+
+        }
+        return maxArea;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,7 +10,22 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        Solution s = new Solution();
+        startTimer();
+        assertEquals(49, s.maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+        stopTimer();
 
+        startTimer();
+        assertEquals(0, s.maxArea(new int[] { 1 }));
+        stopTimer();
+
+        startTimer();
+        assertEquals(1, s.maxArea(new int[] { 1, 1 }));
+        stopTimer();
+
+        startTimer();
+        assertEquals(2, s.maxArea(new int[] { 1, 2, 1 }));
+        stopTimer();
     }
 
     private void startTimer() {
