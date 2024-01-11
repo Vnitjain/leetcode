213231e27171414fbccf4b7c4e639d8fd6279217
Vinit <jain.vinit14@gmail.com>
2024-01-11 13:28:09 -0500

    still incomplete

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,20 @@
 package my.leetcode;
 
-public class Solution {
-    
+class Solution {
+    public int myAtoi(String s) {
+        s.trim();
+        if (s.isEmpty())
+            return 0;
+        int number = 0;
+        int numberLength = 0;
+
+        boolean isPositive = s.charAt(0) != '-';
+
+        for (int i = 0; i < s.length(); i++) {
+            number = number * 10 + Character.getNumericValue(s.charAt(i));
+            numberLength++;
+            if (numberLength > 9 && Integer.MAX_VALUE % 10 < s.charAt(i))
+                return isPositive ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,7 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,7 +10,14 @@ public class AppTest {
 
     @Test
     public void testSolution() {
-
+        Solution s = new Solution();
+        assertEquals(42, s.myAtoi("42"));
+        assertEquals(-42, s.myAtoi(" -42"));
+        assertEquals(4193, s.myAtoi("4193 with words"));
+        assertEquals(2147483647, s.myAtoi("9223372036854775808"));
+        assertEquals(2147483647, s.myAtoi("9223372036854775808"));
+        assertEquals(0, s.myAtoi("words and 987"));
+        assertEquals(2147483647, s.myAtoi("9223372036854775808"));
     }
 
     private void startTimer() {
