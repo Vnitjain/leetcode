f8f50ba29f5611724bc7155dd909e4eeeb2ceacd
Vinit Jain <jain.vinit14@gmail.com>
2024-07-11 17:21:59 -0400

    a bit optimized

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,18 @@
 class Solution:
     def generate(self, numRows):
-        result = []
-        for i in range(numRows):
-            if i == 0:
-                result.append([1])
-            else:
-                tempList = []
-                tempList.append(1)
+        result = [[1]]
+        for i in range(1, numRows):
+            tempList = [1]
+            previousRow = result[i - 1]
+            i = 0
+            while i < len(previousRow) - 1:
+                tempList.append(previousRow[i] + previousRow[i + 1])
+                i += 1
 
-                previousRow = result[i - 1]
-                i = 0
-                while len(previousRow) > 1 and i < len(previousRow) - 1:
-                    tempList.append(previousRow[i] + previousRow[i + 1])
-                    i += 1
-
-                tempList.append(1)
-                result.append(tempList)
+            tempList.append(1)
+            result.append(tempList)
         return result
 
 
 s = Solution()
-print(s.generate(1))
+print(s.generate(5))
