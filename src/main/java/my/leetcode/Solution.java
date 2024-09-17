ded63cff831bc6cfecfb233042fc49563dafd25c
Vinit Jain <jain.vinit14@gmail.com>
2024-09-17 18:20:03 -0400

    solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,35 @@
 package my.leetcode;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Solution {
-    
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return traverseTree(root, 0, targetSum);
+    }
+
+    boolean traverseTree(TreeNode currentNode, int currentSum, int target) {
+        if (currentNode == null)
+            return false;
+        int newSum = currentNode.val + currentSum;
+        if(newSum == target && currentNode.left == null && currentNode.right == null)
+            return true;
+        return traverseTree(currentNode.left, newSum, target) || traverseTree(currentNode.right, newSum, target);
+    }
 }
