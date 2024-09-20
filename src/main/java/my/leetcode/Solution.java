7edf898f7b9a5e6afcb5c8b72182468bbcdbed6a
Vinit Jain <jain.vinit14@gmail.com>
2024-09-19 21:05:39 -0400

    solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,39 @@
 package my.leetcode;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Solution {
-    
+    int max = 0;
+
+    public int maxDepth(TreeNode root) {
+        traverseTree(root, 0);
+        return max;
+    }
+
+    private void traverseTree(TreeNode currentNode, int currentDepth) {
+        if(currentNode==null)
+            return;
+        currentDepth++;
+        if (currentDepth > max)
+            max = currentDepth;
+        traverseTree(currentNode.left, currentDepth);
+        traverseTree(currentNode.right, currentDepth);
+    }
 }
