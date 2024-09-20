bf4ead035a607bc147473f62b1b2200a4a883123
Vinit Jain <jain.vinit14@gmail.com>
2024-09-19 23:44:45 -0400

    solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,39 @@
 package my.leetcode;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Solution {
-    
+    public TreeNode invertTree(TreeNode root) {
+        traverseTree(root);
+        return root;
+    }
+
+    private void traverseTree(TreeNode currentNode) {
+        if (currentNode != null) {
+            TreeNode temp = currentNode.left;
+            currentNode.left = currentNode.right;
+            currentNode.right = temp;
+        } else {
+            return;
+        }
+        traverseTree(currentNode.left);
+        traverseTree(currentNode.right);
+    }
 }
