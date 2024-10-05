06200f4a99660a7ce38fa6ec30050589329d85b6
Vinit Jain <jain.vinit14@gmail.com>
2024-10-05 00:07:44 -0400

    solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,46 @@
 package my.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Solution {
-    
+    List<String> finaList;
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        finaList = new ArrayList<>();
+        traverseTree(root, "");
+        return finaList;
+    }
+
+    private void traverseTree(TreeNode currentNode, String pathString) {
+        if (currentNode == null)
+            return;
+        if (pathString.length() == 0)
+            pathString = String.valueOf(currentNode.val);
+        else
+            pathString += "->" + currentNode.val;
+        if (currentNode.left == null && currentNode.right == null)
+            finaList.add(pathString);
+        traverseTree(currentNode.left, pathString);
+        traverseTree(currentNode.right, pathString);
+    }
 }
