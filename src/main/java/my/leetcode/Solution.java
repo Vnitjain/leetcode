a669bb58bf5bc564b5eda1cf2c68e9cc35e8817f
Vinit Jain <jain.vinit14@gmail.com>
2024-10-05 00:26:31 -0400

    solved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,39 @@
 package my.leetcode;
 
 public class Solution {
-    
+    int sum;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        sum = 0;
+        traverseTree(root);
+        return sum;
+    }
+
+    private void traverseTree(TreeNode currentNode) {
+        if (currentNode == null)
+            return;
+        if (currentNode.left != null && currentNode.left.left == null && currentNode.left.right == null)
+            sum += currentNode.left.val;
+        traverseTree(currentNode.left);
+        traverseTree(currentNode.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
