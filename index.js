31d54d7560bae4cb86b5931bd56788aa68cfe2e1
Vinit Jain <jain.vinit14@gmail.com>
2025-02-11 23:28:06 -0500

    solved

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var maximumSum = function (nums) {
+  let max = -1;
+  const count = new Map();
+  for (const num of nums) {
+    const digitSum = String(num)
+      .split("")
+      .reduce((acc, curr) => acc + parseInt(curr), 0);
+    const prev = count.get(digitSum) ?? 0;
+    if (count.get(digitSum)) {
+      max = Math.max(prev + num, max);
+    }
+    count.set(digitSum, Math.max(prev, num));
+  }
+  return max;
+};
 
+console.log(maximumSum([18, 43, 36, 13, 7]));
