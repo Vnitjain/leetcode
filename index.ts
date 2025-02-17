d5d4c0fa5a99130fee77eb5e23440dc0674e74ff
Vinit Jain <jain.vinit14@gmail.com>
2025-02-17 14:59:19 -0500

    resolved

---
+++ index.ts
@@ -0,0 +1,24 @@
+function numTilePossibilities(tiles: string): number {
+  let finalList = new Set<string>();
+  traverseTiles("", finalList, tiles, new Array(tiles.length).fill(0));
+  return finalList.size;
+}
+
+function traverseTiles(
+  currentString: string,
+  finalList: Set<string>,
+  tiles: string,
+  visited: number[]
+) {
+  for (let i = 0; i < tiles.length; i++) {
+    if (visited[i] === 0) {
+      const newVisited = Array.from(visited);
+      newVisited[i] = 1;
+      const newString = currentString + tiles[i];
+      finalList.add(newString);
+      traverseTiles(newString, finalList, tiles, newVisited);
+    }
+  }
+}
+
+console.log(numTilePossibilities("AAB"));
