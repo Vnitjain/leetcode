dee0196d9426ea069bb5c33d8a39157cd1350df8
Vinit Jain <jain.vinit14@gmail.com>
2025-02-16 20:50:06 -0500

    reset the index.js file to blank and removed all the test files

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function timeConversion(s) {
-  let [hours, min, sec] = s.split(":");
-  let ampm = sec.slice(2);
-  sec = sec.slice(0, 2);
-  if (ampm == "PM") {
-    if (hours !== "12") hours = String(parseInt(hours) + 12);
-  } else {
-    if (hours === "12") {
-      hours = "00";
-    }
-  }
-  return [hours, min, sec].join(":");
-}
 
-timeConversion("07:05:45PM");
--- index.test.ts
+++
@@ -1,5 +0,0 @@
-describe("test", () => {
-  it("should return 5", () => {
-    expect(true);
-  });
-});
