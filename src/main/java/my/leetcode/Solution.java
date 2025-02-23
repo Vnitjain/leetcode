73e93b4f813aa63b1fd03f657d4537bb2d82d1e8
Vinit Jain <jain.vinit14@gmail.com>
2025-02-23 17:06:05 -0500

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,22 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public String reverseVowels(String s) {
+        String vowels = "aeiouAEIOU";
+        char[] strArr = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && vowels.indexOf(strArr[i]) == -1)
+                i++;
+            while (i < j && vowels.indexOf(strArr[j]) == -1)
+                j--;
+
+            char temp = strArr[i];
+            strArr[i] = strArr[j];
+            strArr[j] = temp;
+            i++;
+            j--;
+        }
+        return new String(strArr);
+    }
 }
