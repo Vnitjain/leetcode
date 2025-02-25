0cd4b372301166e72f9c75ee45f9158a040df560
Vinit Jain <jain.vinit14@gmail.com>
2025-02-25 12:26:33 -0500

    resolved

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -4,10 +4,13 @@ public class Solution {
     public boolean increasingTriplet(int[] nums) {
         int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
         for (int num : nums) {
-            first = num < first ? num : first;
-            second = num < second && num > first ? num : second;
-            if (num > second)
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
                 return true;
+            }
         }
         return false;
     }
