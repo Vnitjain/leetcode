51d7ce918aa7a6210bc96047c56b1452b6474a1d
Vinit Jain <jain.vinit14@gmail.com>
2025-02-25 11:22:36 -0500

    doesn't cover all the test cases

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
+import java.util.Stack;
+
 public class Solution {
-    
+    public boolean increasingTriplet(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        while (i < nums.length) {
+            if (stack.size() == 3)
+                return true;
+            while (!stack.empty() && stack.peek() > nums[i]) {
+                stack.pop();
+            }
+            stack.add(nums[i++]);
+        }
+        return stack.size() >= 3;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,5 +1,8 @@
 package my.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class AppTest {
@@ -8,7 +11,22 @@ public class AppTest {
 
     @Test
     public void testSolution() {
+        assertTrue(s.increasingTriplet(new int[] { 1, 2, 3, 4, 5 }));
+    }
 
+    @Test
+    public void testSolution1() {
+        assertFalse(s.increasingTriplet(new int[] { 5, 4, 3, 2, 1 }));
+    }
+
+    @Test
+    public void testSolution2() {
+        assertTrue(s.increasingTriplet(new int[] { 2, 1, 5, 0, 4, 6 }));
+    }
+
+    @Test
+    public void testSolution3() {
+        assertTrue(s.increasingTriplet(new int[] { 20, 100, 10, 12, 5, 13 }));
     }
 
     private void startTimer() {
