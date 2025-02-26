f4a928ad8008fad056e11c9f1d6be938b5464abf
Vinit Jain <jain.vinit14@gmail.com>
2025-02-25 20:52:38 -0500

    solved with java

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0)
+            return true;
+        int i = 0, j = 0;
+
+        while (j < t.length()) {
+            if (i == s.length())
+                return true;
+            if (s.charAt(i) == t.charAt(j))
+                i++;
+
+            j++;
+        }
+        return i == s.length();
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,40 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testExample1() {
+        assertTrue(s.isSubsequence("abc", "ahbgdc"));
+    }
 
+    @Test
+    public void testExample2() {
+        assertFalse(s.isSubsequence("axc", "ahbgdc"));
+    }
+
+    @Test
+    public void testEmptyS() {
+        assertTrue(s.isSubsequence("", "ahbgdc"));
+    }
+
+    @Test
+    public void testEmptyT() {
+        assertFalse(s.isSubsequence("abc", ""));
+    }
+
+    @Test
+    public void testSameString() {
+        assertTrue(s.isSubsequence("abc", "abc"));
+    }
+
+    @Test
+    public void testLongerS() {
+        assertFalse(s.isSubsequence("ahbgdc", "abc"));
     }
 
     private void startTimer() {
@@ -18,4 +44,5 @@ public class AppTest {
     private void stopTimer() {
         System.out.println("Code Executed in : " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
+
 }
