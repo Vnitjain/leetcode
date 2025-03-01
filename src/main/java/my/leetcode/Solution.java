03163795b88df38198443a0d5729f60d3254f095
Vinit Jain <jain.vinit14@gmail.com>
2025-02-28 22:58:25 -0500

    resolved 61%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,22 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public double findMaxAverage(int[] nums, int k) {
+        if (k <= 0 || nums.length < k)
+            return 0;
+
+        double max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        max = (double) sum / k;
+
+        for (int i = k; i < nums.length; i++) {
+            sum = sum - nums[i - k] + nums[i];
+            max = Math.max(max, (double) sum / k);
+        }
+        return max;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,54 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testExample1() {
+        startTimer();
+        double result = s.findMaxAverage(new int[] { 1, 12, -5, -6, 50, 3 }, 4);
+        stopTimer();
+        assertEquals(12.75, result, 0.00001);
+    }
 
+    @Test
+    public void testExample2() {
+        startTimer();
+        double result = s.findMaxAverage(new int[] { 5 }, 1);
+        stopTimer();
+        assertEquals(5.0, result, 0.00001);
+    }
+
+    @Test
+    public void testEmptyArray() {
+        startTimer();
+        double result = s.findMaxAverage(new int[] {}, 0);
+        stopTimer();
+        assertEquals(0.0, result, 0.00001);
+    }
+
+    @Test
+    public void testLargeArray() {
+        startTimer();
+        int[] nums = new int[100000];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+        }
+        double result = s.findMaxAverage(nums, 1000);
+        stopTimer();
+        // assertion not added as the result varies based on implementation
+    }
+
+    @Test
+    public void testNegativeArray() {
+        startTimer();
+        double result = s.findMaxAverage(new int[] { -1 }, 1);
+        stopTimer();
+        assertEquals(-1.0, result, 0.00001);
     }
 
     private void startTimer() {
