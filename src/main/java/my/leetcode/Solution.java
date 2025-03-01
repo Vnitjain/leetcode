a9cfa54c597fdfa2b49bee761bacfe3bf4be7f12
Vinit Jain <jain.vinit14@gmail.com>
2025-02-28 21:12:59 -0500

    resolved 14%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,22 @@
 package my.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
-    
+    public int maxOperations(int[] nums, int k) {
+        int operations = 0;
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int difference = k - current;
+            if (count.containsKey(difference) && count.get(difference) > 0) {
+                count.put(difference, count.get(difference) - 1);
+                operations++;
+            } else {
+                count.put(current, count.getOrDefault(current, 0) + 1);
+            }
+        }
+        return operations;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,13 +1,55 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
+    public void testSolutionExample1() {
+        int[] nums = { 1, 2, 3, 4 };
+        int k = 5;
+        int expected = 2;
+        int actual = s.maxOperations(nums, k);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolutionExample2() {
+        int[] nums = { 3, 1, 3, 4, 3 };
+        int k = 6;
+        int expected = 1;
+        int actual = s.maxOperations(nums, k);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolutionEmptyArray() {
+        int[] nums = {};
+        int k = 5;
+        int expected = 0;
+        int actual = s.maxOperations(nums, k);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolutionNoPairs() {
+        int[] nums = { 1, 2, 3 };
+        int k = 10;
+        int expected = 0;
+        int actual = s.maxOperations(nums, k);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void testSolution() {
+        int[] nums = { 3, 5, 1, 5 };
+        int k = 2;
+        int expected = 0;
+        int actual = s.maxOperations(nums, k);
+        assertEquals(expected, actual);
 
     }
 
