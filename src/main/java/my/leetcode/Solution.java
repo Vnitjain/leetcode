7dd93c1ce969c5e4498ef005bbb1e6bce2fc93ea
Vinit Jain <jain.vinit14@gmail.com>
2025-03-02 01:56:27 -0500

    resolved 53%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,25 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public int maxVowels(String s, int k) {
+        int currentStringVowels = 0;
+        String vowels = "aeiou";
+        int maxVowels = Integer.MIN_VALUE;
+        for (int i = 0; i < k; i++)
+            if (vowels.indexOf(s.charAt(i)) > -1)
+                currentStringVowels++;
+
+        maxVowels = Math.max(maxVowels, currentStringVowels);
+
+        for (int i = k; i < s.length(); i++) {
+            if (vowels.indexOf(s.charAt(i - k)) > -1)
+                currentStringVowels--;
+
+            if (vowels.indexOf(s.charAt(i)) > -1)
+                currentStringVowels++;
+
+            maxVowels = Math.max(maxVowels, currentStringVowels);
+        }
+        return maxVowels;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,37 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testExample1() {
+        assertEquals(3, s.maxVowels("abciiidef", 3));
+    }
 
+    @Test
+    public void testExample2() {
+        assertEquals(2, s.maxVowels("aeiou", 2));
+    }
+
+    @Test
+    public void testExample3() {
+        assertEquals(2, s.maxVowels("leetcode", 3));
+    }
+
+    @Test
+    public void testOneChar() {
+        assertEquals(1, s.maxVowels("a", 1));
+        assertEquals(0, s.maxVowels("b", 1));
+    }
+
+    @Test
+    public void testLongString() {
+        String longString = "aeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiouaeiou";
+        assertEquals(5, s.maxVowels(longString, 5));
     }
 
     private void startTimer() {
