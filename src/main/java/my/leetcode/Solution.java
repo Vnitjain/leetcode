95789d694277345fa6589002e6f4cc6f1294a611
Vinit Jain <jain.vinit14@gmail.com>
2025-03-02 19:23:55 -0500

    resolved 88%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,23 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public int longestOnes(int[] nums, int k) {
+        int i = 0, j = 0, zeroCount = 0;
+        int max = Integer.MIN_VALUE;
+        while (j < nums.length) {
+            int currentNumber = nums[j];
+            if (currentNumber == 0)
+                zeroCount++;
+
+            while (zeroCount > k) {
+                if (nums[i] == 0)
+                    zeroCount--;
+                i++;
+            }
+            max = Math.max(max, j - i + 1);
+
+            j++;
+        }
+        return max;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,28 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testSolutionExample1() {
+        int[] nums = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+        int k = 2;
+        int expected = 6;
+        int actual = s.longestOnes(nums, k);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testSolutionExample2() {
+        int[] nums = { 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1 };
+        int k = 3;
+        int expected = 10;
+        int actual = s.longestOnes(nums, k);
+        assertEquals(expected, actual);
     }
 
     private void startTimer() {
