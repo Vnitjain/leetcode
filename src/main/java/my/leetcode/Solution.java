c030cabb3c0a99c9f6017ee1ea3450ab5789c0d1
Vinit Jain <jain.vinit14@gmail.com>
2025-03-02 19:53:07 -0500

    resolved 78%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public int longestSubarray(int[] nums) {
+        int i = 0, maxCount = Integer.MIN_VALUE, zeroCount = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] == 0)
+                zeroCount++;
+
+            while (zeroCount > 1) {
+                if (nums[i] == 0)
+                    zeroCount--;
+                i++;
+            }
+            maxCount = Math.max(maxCount, j - i);
+        }
+        return maxCount;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,34 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testExample1() {
+        int[] nums = { 1, 1, 0, 1 };
+        int expected = 3;
+        int actual = s.longestSubarray(nums);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testExample2() {
+        int[] nums = { 0, 1, 1, 1, 0, 1, 1, 0, 1 };
+        int expected = 5;
+        int actual = s.longestSubarray(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExample3() {
+        int[] nums = { 1, 1, 1 };
+        int expected = 2;
+        int actual = s.longestSubarray(nums);
+        assertEquals(expected, actual);
     }
 
     private void startTimer() {
