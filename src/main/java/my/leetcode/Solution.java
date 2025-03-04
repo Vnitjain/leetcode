f2b568ce387f97e6b7aeecc3d4bc2ec27c9562b1
Vinit Jain <jain.vinit14@gmail.com>
2025-03-04 03:11:39 -0500

    solved 94%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,20 @@
 package my.leetcode;
 
 public class Solution {
-    
+    public int pivotIndex(int[] nums) {
+        int length = nums.length;
+        int[] prefix = new int[length];
+        int[] postfix = new int[length];
+        for (int i = 0; i < length; i++)
+            prefix[i] = i == 0 ? 0 + nums[i] : prefix[i - 1] + nums[i];
+        for (int i = length - 1; i > -1; i--)
+            postfix[i] = i == length - 1 ? 0 + nums[i] : nums[i] + postfix[i + 1];
+        for (int i = 0; i < length; i++) {
+            int pref = i == 0 ? 0 : prefix[i - 1];
+            int postf = i == length - 1 ? 0 : postfix[i + 1];
+            if (pref == postf)
+                return i;
+        }
+        return -1;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,34 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testSolution() {
+    public void testSolution1() {
+        int[] nums = { 1, 7, 3, 6, 5, 6 };
+        int expected = 3;
+        int actual = s.pivotIndex(nums);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testSolution2() {
+        int[] nums = { 1, 2, 3 };
+        int expected = -1;
+        int actual = s.pivotIndex(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSolution3() {
+        int[] nums = { 2, 1, -1 };
+        int expected = 0;
+        int actual = s.pivotIndex(nums);
+        assertEquals(expected, actual);
     }
 
     private void startTimer() {
