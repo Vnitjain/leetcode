19032f938ba19c8de8343b861e36ba2126309040
Vinit Jain <jain.vinit14@gmail.com>
2025-03-04 23:49:21 -0500

    reverted changes

--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,28 +1,14 @@
 package my.leetcode;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void testExample1() {
-        int[] arr = { 1, 2, 2, 1, 1, 3 };
-        assertTrue(s.uniqueOccurrences(arr));
-    }
+    public void test() {
 
-    @Test
-    public void testExample2() {
-        int[] arr = { 1, 2 };
-        assertFalse(s.uniqueOccurrences(arr));
-    }
-
-    @Test
-    public void testExample3() {
-        int[] arr = { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 };
-        assertTrue(s.uniqueOccurrences(arr));
     }
 
     private void startTimer() {
