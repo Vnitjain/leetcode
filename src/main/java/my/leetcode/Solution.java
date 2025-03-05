ced86aa76de4ff081b1ae7d5bf336636f86ff28f
Vinit Jain <jain.vinit14@gmail.com>
2025-03-04 23:49:51 -0500

    solved 98%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,21 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
+public class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> count = new HashMap<>();
+        Set<Integer> freq = new HashSet<>();
+        for (int i : arr)
+            count.put(i, count.getOrDefault(i, 1) + 1);
+        for (int f : count.values()) {
+            if (freq.contains(f))
+                return false;
+            freq.add(f);
+        }
+        return true;
+    }
 }
