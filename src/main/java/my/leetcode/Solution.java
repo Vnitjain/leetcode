b824554b9cb9fcdfd85e9a99817c773aa397e4a4
Vinit Jain <jain.vinit14@gmail.com>
2025-03-08 16:54:15 -0500

    solved 85%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,25 +1,22 @@
 package my.leetcode;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Solution {
     public boolean closeStrings(String word1, String word2) {
-        Map<Character, Integer> freq1 = new HashMap<>();
-        Map<Character, Integer> freq2 = new HashMap<>();
-        for (char c : word1.toCharArray())
-            freq1.put(c, freq1.getOrDefault(c, 0) + 1);
-        for (char c : word2.toCharArray())
-            freq2.put(c, freq2.getOrDefault(c, 0) + 1);
-        if (!freq1.keySet().equals(freq2.keySet()))
-            return false;
-        Map<Integer, Integer> freq1Freq = new HashMap<>();
-        for (int i : freq1.values()) {
-            freq1Freq.put(i, freq1Freq.getOrDefault(i, 0) + 1);
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for (char c : word1.toCharArray()) {
+            freq1[(int) c - 97] += 1;
         }
-        for (int i : freq2.values()) {
-            freq1Freq.put(i, freq1Freq.getOrDefault(i, 0) - 1);
+        for (char c : word2.toCharArray()) {
+            freq2[(int) c - 97] += 1;
         }
-        return freq1Freq.values().stream().allMatch((i) -> i == 0);
+        for (int i = 0; i < 26; i++)
+            if ((freq1[i] > 0 && freq2[i] == 0) || (freq1[i] == 0 && freq2[i] > 0))
+                return false;
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+        return Arrays.equals(freq1, freq2);
     }
 }
