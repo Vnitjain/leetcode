b2f95ac9547afd86c7b97b0789af073215907bb6
Vinit Jain <jain.vinit14@gmail.com>
2025-03-08 16:32:30 -0500

    solved 26%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,25 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        Map<Character, Integer> freq1 = new HashMap<>();
+        Map<Character, Integer> freq2 = new HashMap<>();
+        for (char c : word1.toCharArray())
+            freq1.put(c, freq1.getOrDefault(c, 0) + 1);
+        for (char c : word2.toCharArray())
+            freq2.put(c, freq2.getOrDefault(c, 0) + 1);
+        if (!freq1.keySet().equals(freq2.keySet()))
+            return false;
+        Map<Integer, Integer> freq1Freq = new HashMap<>();
+        for (int i : freq1.values()) {
+            freq1Freq.put(i, freq1Freq.getOrDefault(i, 0) + 1);
+        }
+        for (int i : freq2.values()) {
+            freq1Freq.put(i, freq1Freq.getOrDefault(i, 0) - 1);
+        }
+        return freq1Freq.values().stream().allMatch((i) -> i == 0);
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,38 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void test() {
+    public void testExample1() {
+        startTimer();
+        assertEquals(true, s.closeStrings("abc", "bca"));
+        stopTimer();
+    }
 
+    @Test
+    public void testExample2() {
+        startTimer();
+        assertEquals(false, s.closeStrings("a", "aa"));
+        stopTimer();
+    }
+
+    @Test
+    public void testExample3() {
+        startTimer();
+        assertEquals(true, s.closeStrings("cabbba", "abbccc"));
+        stopTimer();
+    }
+
+    @Test
+    public void testExample4() {
+        startTimer();
+        assertEquals(false, s.closeStrings("aaabbbbccddeeeeefffff", "aaaaabbcccdddeeeeffff"));
+        stopTimer();
     }
 
     private void startTimer() {
