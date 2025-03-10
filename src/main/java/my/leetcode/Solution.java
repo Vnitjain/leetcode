52fbe01e4e85e698bd410c5a0bc399db97b54ea9
Vinit Jain <jain.vinit14@gmail.com>
2025-03-10 12:33:35 -0400

    solved 80%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,30 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Solution {
+    public int equalPairs(int[][] grid) {
+        Map<List<Integer>, Integer> count = new HashMap<>();
+        int matches = 0;
+        for (int i = 0; i < grid.length; i++) {
+            List<Integer> rowList = new ArrayList<>();
+            for (int j = 0; j < grid[i].length; j++) {
+                rowList.add(grid[i][j]);
+            }
+            count.put(rowList, count.getOrDefault(rowList, 0) + 1);
+        }
+        for (int i = 0; i < grid.length; i++) {
+            List<Integer> columnList = new ArrayList<>();
+            for (int j = 0; j < grid[i].length; j++) {
+                columnList.add(grid[j][i]);
+            }
+            if (count.containsKey(columnList)) {
+                matches += count.getOrDefault(columnList, 0);
+            }
+        }
+        return matches;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,34 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void test() {
+    public void testExample1() {
+        int[][] grid = { { 3, 2, 1 }, { 1, 7, 6 }, { 2, 7, 7 } };
+        int expected = 1;
+        int actual = s.equalPairs(grid);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void testExample2() {
+        int[][] grid = { { 3, 1, 2, 2 }, { 1, 4, 4, 5 }, { 2, 4, 2, 2 }, { 2, 4, 2, 2 } };
+        int expected = 3;
+        int actual = s.equalPairs(grid);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEmptyGrid() {
+        int[][] grid = {};
+        int expected = 0;
+        int actual = s.equalPairs(grid);
+        assertEquals(expected, actual);
     }
 
     private void startTimer() {
