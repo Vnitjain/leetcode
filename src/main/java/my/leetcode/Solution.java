4a5bab8ac2a907d70d25f8125f438b52525eb100
Vinit Jain <jain.vinit14@gmail.com>
2025-03-10 13:40:28 -0400

    solved 13%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Stack;
 
+public class Solution {
+    public String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character character : s.toCharArray())
+            if (character >= 'a' && character <= 'z')
+                stack.push(character);
+            else
+                stack.pop();
+        String finalString = "";
+        for (Character character : stack)
+            finalString += String.valueOf(character);
+
+        return finalString;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,50 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void test() {
+    public void testExample1() {
+        startTimer();
+        String input = "leet**cod*e";
+        String expected = "lecoe";
+        String actual = s.removeStars(input);
+        assertEquals(expected, actual);
+        stopTimer();
+    }
 
+    @Test
+    public void testExample2() {
+        startTimer();
+        String input = "erase*****";
+        String expected = "";
+        String actual = s.removeStars(input);
+        assertEquals(expected, actual);
+        stopTimer();
+    }
+
+    @Test
+    public void testEmptyInput() {
+        startTimer();
+        String input = "";
+        String expected = "";
+        String actual = s.removeStars(input);
+        assertEquals(expected, actual);
+        stopTimer();
+    }
+
+    @Test
+    public void testNoStars() {
+        startTimer();
+        String input = "leetcode";
+        String expected = "leetcode";
+        String actual = s.removeStars(input);
+        assertEquals(expected, actual);
+        stopTimer();
     }
 
     private void startTimer() {
@@ -18,4 +54,5 @@ public class AppTest {
     private void stopTimer() {
         System.out.println("Code Executed in : " + (System.currentTimeMillis() - startTime) + " milliseconds");
     }
+
 }
