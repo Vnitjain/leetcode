26b0700376926a897fc9ca0045fc8b67279737cb
Vinit Jain <jain.vinit14@gmail.com>
2025-03-22 21:51:28 -0400

    resolved 13.31%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,31 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Stack;
 
+public class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (Character c : s.toCharArray())
+            if (c == ']') {
+                String temp = "";
+                while (!stack.isEmpty() && stack.peek() != '[')
+                    temp = stack.pop() + temp;
+                stack.pop();
+                String tempCount = "";
+                while (!stack.empty() && Character.isDigit(stack.peek()))
+                    tempCount = stack.pop() + tempCount;
+                String tempString = "";
+                for (int i = 0; i < Integer.valueOf(tempCount); i++)
+                    tempString += temp;
+                tempString.chars().forEach((i) -> {
+                    stack.add((char) i);
+                });
+            } else
+                stack.add(c);
+        String finalString = "";
+        while (!stack.isEmpty()) {
+            finalString = stack.pop() + finalString;
+        }
+        return finalString;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,34 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void test() {
+    public void test1() {
+        String input = "3[a]2[bc]";
+        String expected = "aaabcbc";
+        String actual = s.decodeString(input);
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void test2() {
+        String input = "3[a2[c]]";
+        String expected = "accaccacc";
+        String actual = s.decodeString(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        String input = "2[abc]3[cd]ef";
+        String expected = "abcabccdcdcdef";
+        String actual = s.decodeString(input);
+        assertEquals(expected, actual);
     }
 
     private void startTimer() {
