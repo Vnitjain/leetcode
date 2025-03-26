1aeca28f45fcf5f48b2540c2080c7aff819b53a9
Vinit Jain <jain.vinit14@gmail.com>
2025-03-25 23:01:35 -0400

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,41 @@
 package my.leetcode;
 
-public class Solution {
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode p = head;
+        int length = 0;
+        while (p != null) {
+            p = p.next;
+            length++;
+        }
+        if (length < 2) {
+            return null;
+        }
+        int mid = length / 2;
+        p = head;
+        ListNode prev = p;
+        for (int i = 0; i < mid; i++) {
+            prev = p;
+            p = p.next;
+        }
+        prev.next = p.next;
+        return head;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,21 +1,46 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
     Solution s = new Solution();
-    long startTime;
 
     @Test
-    public void test() {
-
+    public void test1() {
+        ListNode head = new ListNode(1,
+                new ListNode(3, new ListNode(4, new ListNode(7, new ListNode(1, new ListNode(2, new ListNode(6)))))));
+        ListNode expected = new ListNode(1,
+                new ListNode(3, new ListNode(4, new ListNode(1, new ListNode(2, new ListNode(6))))));
+        ListNode result = s.deleteMiddle(head);
+        assertEquals(listToString(expected), listToString(result));
     }
 
-    private void startTimer() {
-        startTime = System.currentTimeMillis();
+    @Test
+    public void test2() {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+        ListNode expected = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode result = s.deleteMiddle(head);
+        assertEquals(listToString(expected), listToString(result));
     }
 
-    private void stopTimer() {
-        System.out.println("Code Executed in : " + (System.currentTimeMillis() - startTime) + " milliseconds");
+    @Test
+    public void test3() {
+        ListNode head = new ListNode(2, new ListNode(1));
+        ListNode expected = new ListNode(2);
+        ListNode result = s.deleteMiddle(head);
+        assertEquals(listToString(expected), listToString(result));
+    }
+
+    private String listToString(ListNode head) {
+        StringBuilder sb = new StringBuilder("[");
+        while (head != null) {
+            sb.append(head.val);
+            if (head.next != null)
+                sb.append(",");
+            head = head.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
