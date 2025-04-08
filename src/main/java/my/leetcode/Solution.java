1e8731b81e62661c0cfc108b79c01a3ad1659576
Vinit Jain <jain.vinit14@gmail.com>
2025-04-08 09:44:29 -0400

    resolved 66%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,17 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Arrays;
 
+class Solution {
+    public long dividePlayers(int[] skill) {
+        Arrays.sort(skill);
+        int prev = skill[0] + skill[skill.length - 1];
+        long finalLong = skill[0] * skill[skill.length - 1];
+        for (int i = 1; i < skill.length / 2; i++) {
+            if (skill[i] + skill[skill.length - 1 - i] != prev)
+                return -1;
+            finalLong += skill[i] * skill[skill.length - 1 - i];
+        }
+        return finalLong;
+    }
 }
