a73f35a2ccdb479c62f32c953cdd2ead8e399950
Vinit Jain <jain.vinit14@gmail.com>
2025-04-19 22:24:10 -0400

    solved 97%

--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -1,14 +1,24 @@
 package my.leetcode;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class AppTest {
     Solution s = new Solution();
     long startTime;
 
     @Test
-    public void test() {
+    public void testExample1() {
+        int[] nums = { 1, 4, 3, 2 };
+        int expected = 4;
+        assertEquals(expected, s.arrayPairSum(nums));
+    }
 
+    @Test
+    public void testExample2() {
+        int[] nums = { 6, 2, 6, 5, 1, 2 };
+        int expected = 9;
+        assertEquals(expected, s.arrayPairSum(nums));
     }
 
     private void startTimer() {
