cc72075351894ca014859b9840bc669c8c6afb7a
Vinit Jain <jain.vinit14@gmail.com>
2025-04-23 00:06:41 -0400

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,40 @@
 package my.leetcode;
 
-public class Solution {
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode evenStart = head;
+        ListNode oddStart = head.next;
+
+        ListNode i = evenStart;
+        ListNode j = oddStart;
+        while (i.next != null && j.next != null) {
+
+            i.next = i.next.next;
+            i = i.next;
+
+            j.next = j.next.next;
+            j = j.next;
+        }
+        i.next = oddStart;
+        return evenStart;
+    }
 }
