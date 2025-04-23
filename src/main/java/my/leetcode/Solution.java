82912fd27f953604edf3102cef04bfa5e0b2ba6a
Vinit Jain <jain.vinit14@gmail.com>
2025-04-23 18:54:57 -0400

    solved 97%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -21,21 +21,22 @@ class ListNode {
 
 public class Solution {
     public int pairSum(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
         ListNode pointer = head;
         int length = 0;
         while (pointer != null) {
             length++;
             pointer = pointer.next;
         }
+        int[] stack = new int[length];
+        int stackTop = -1;
         pointer = head;
         for (int i = 0; i < length / 2; i++) {
-            stack.push(pointer.val);
+            stack[++stackTop] = pointer.val;
             pointer = pointer.next;
         }
         int max = Integer.MIN_VALUE;
         while (pointer != null) {
-            max = Math.max(max, pointer.val + stack.pop());
+            max = Math.max(max, pointer.val + stack[stackTop--]);
             pointer = pointer.next;
         }
         return max;
