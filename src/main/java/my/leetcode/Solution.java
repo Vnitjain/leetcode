93dabb0493ee37fde26b6ccbfa73f2ac0499411b
Vinit Jain <jain.vinit14@gmail.com>
2025-04-23 00:20:07 -0400

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,35 @@
 package my.leetcode;
 
-public class Solution {
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode pointer = head;
+        ListNode prev = null;
+        while (pointer.next != null) {
+            ListNode next = pointer.next;
+            pointer.next = prev;
+            prev = pointer;
+            pointer = next;
+        }
+        pointer.next = prev;
+        return pointer;
+    }
 }
