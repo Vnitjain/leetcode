38360338b45a2607623a9ce0f2aebec02cef22a7
Vinit Jain <jain.vinit14@gmail.com>
2025-05-16 12:06:40 -0400

    inital code

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,37 @@
 package my.leetcode;
 
-public class Solution {
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    int maxLength = 0;
+
+    public int longestZigZag(TreeNode root) {
+        traverseTree(root, 0, 0);
+    }
+
+    private int traverseTree(TreeNode root, int carry, int previous) {
+        if (root != null) {
+            if (previous == -1) {
+            }
+            traverseTree(root.left, 1);
+            traverseTree(root.right, 1);
+        }
+    }
 }
