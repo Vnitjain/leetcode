333e4acc77229ab6b1b02eb2646c03b7be282d87
Vinit Jain <jain.vinit14@gmail.com>
2025-05-19 19:22:48 -0400

    solved 98%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,43 @@
 package my.leetcode;
 
-public class Solution {
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    int maxCount = 0;
+
+    public int longestZigZag(TreeNode root) {
+        traverseTree(root.left, true, 1);
+        traverseTree(root.right, false, 1);
+        return maxCount;
+    }
+
+    private void traverseTree(TreeNode currentNode, boolean isLeft, int count) {
+        if (currentNode == null)
+            return;
+        if (isLeft) {
+            traverseTree(currentNode.left, true, 1);
+            traverseTree(currentNode.right, false, count + 1);
+        } else {
+            traverseTree(currentNode.left, true, count + 1);
+            traverseTree(currentNode.right, false, 1);
+        }
+        maxCount = Math.max(maxCount, count);
+    }
 }
