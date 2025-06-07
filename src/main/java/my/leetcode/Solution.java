87220003f16ae1d756a350f9de5270eff07e9792
Vinit Jain <jain.vinit14@gmail.com>
2025-06-07 13:24:13 -0400

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,40 @@
 package my.leetcode;
 
-public class Solution {
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        return traverseTree(root, val);
+    }
+
+    private TreeNode traverseTree(TreeNode root, int val) {
+        if (root == null)
+            return root;
+        if (root.val == val)
+            return root;
+        TreeNode left = traverseTree(root.left, val);
+        if (left != null)
+            return left;
+        TreeNode right = traverseTree(root.right, val);
+        if (right != null)
+            return right;
+        return null;
+    }
 }
