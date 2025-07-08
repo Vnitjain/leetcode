2a079c55e5831136e7f70bd931b6146760f069ca
Vinit Jain <jain.vinit14@gmail.com>
2025-07-08 04:57:19 -0400

    solved 100%

--- index.ts
+++ index.ts
@@ -1,33 +1,35 @@
 class TreeNode {
-      val: number
-      left: TreeNode | null
-      right: TreeNode | null
-      constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-          this.val = (val===undefined ? 0 : val)
-          this.left = (left===undefined ? null : left)
-          this.right = (right===undefined ? null : right)
-      }
-  }
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
 
-function traverseNodes(root:TreeNode|null,currentLevel:number,levelSum:Map<number,number>):void{
-    if(root===null)
+function traverseNodes(root: TreeNode | null, currentLevel: number, levelSumArr: number[]): void {
+    if (root === null)
         return;
 
-    levelSum.set(currentLevel,levelSum.has(currentLevel)?levelSum.get(currentLevel)+root.val:root.val);
+    levelSumArr[currentLevel] = levelSumArr[currentLevel] ? levelSumArr[currentLevel] + root.val : root.val;
 
-    traverseNodes(root.left,currentLevel+1,levelSum);
-    traverseNodes(root.right,currentLevel+1,levelSum);
+    traverseNodes(root.left, currentLevel + 1, levelSumArr);
+    traverseNodes(root.right, currentLevel + 1, levelSumArr);
 }
 
 function maxLevelSum(root: TreeNode | null): number {
-    const levelSum:Map<number,number> = new Map<number, number>();
+    const levelSumArr: number[] = [];
+    if (root === null)
+        return -1;
     let maxValue = root?.val;
     let maxKey = 1;
-    traverseNodes(root,1,levelSum);
-    for (const [key,value] of levelSum){
-        if(value>maxValue) {
+    traverseNodes(root, 1, levelSumArr);
+    for (const [index, value] of levelSumArr.entries()) {
+        if (value > maxValue) {
             maxValue = value;
-            maxKey = key;
+            maxKey = index;
         }
     }
     return maxKey;
