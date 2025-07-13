632afeb400d8a1ac0d6a9865bd160b0b7f4d7e06
Vinit Jain <jain.vinit14@gmail.com>
2025-07-13 00:01:52 -0400

    solved 99%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,29 @@
 package my.leetcode;
 
 public class Solution {
-
+public String sortString(String s) {
+        StringBuilder result = new StringBuilder();
+        int[] frequency = new int[26];
+        int length = s.length();
+        for (char c : s.toCharArray()) {
+           frequency[(int)c - 97]++; 
+        }
+        while (length>0) {
+            for (int i = 0; i < 26; i++) {
+                if(frequency[i]!=0){
+                    frequency[i]--;
+                    result.append((char)(i+97));
+                    length--;
+                }
+            }
+            for (int i = 25; i > -1; i--) {
+                if(frequency[i]!=0){
+                    frequency[i]--;
+                    result.append((char)(i+97));
+                    length--;
+                }
+            }
+        }
+        return result.toString();
+    }
 }
