1d784ab1259b56a37db5b4e1b4dd1155aa51316d
Vinit Jain <jain.vinit14@gmail.com>
2025-07-14 21:18:51 -0400

    solved 30%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,33 @@
 package my.leetcode;
 
 public class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3)
+            return false;
 
+        word = word.toLowerCase();
+        boolean hasConsonant = false;
+        boolean hasVowel = false;
+        for (int i = 0; i < word.length(); i++) {
+            char charAt = word.charAt(i);
+            if (Character.isAlphabetic(charAt)) {
+                if (isVowel(charAt)) {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            } else if (Character.isDigit(charAt)) {
+
+            } else {
+                return false;
+            }
+        }
+        return hasConsonant && hasVowel;
+    }
+
+    private boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            return true;
+        return false;
+    }
 }
