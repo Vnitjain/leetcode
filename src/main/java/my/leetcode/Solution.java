27f4207e6101e74605a1645d4274bdc2b0de87fa
Vinit Jain <jain.vinit14@gmail.com>
2025-07-15 19:46:56 -0400

    resolved 95%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution {
+ public int firstUniqChar(String s) {
+        int[] map = new int[26];
+
+        for(char c : s.toCharArray()){
+            map[(int)c-97]++;
+        }
+        for(int i=0;i<s.length();i++)
+            if(map[(int)s.charAt(i)-97]==1)
+                return i;
+
+        return -1;
+    }
 }
