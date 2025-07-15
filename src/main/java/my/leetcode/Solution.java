a52f2903a5836ee3a18da20b68f6a8151903a28a
Vinit Jain <jain.vinit14@gmail.com>
2025-07-15 19:30:56 -0400

    solveed 44%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,15 @@
 package my.leetcode;
 
 public class Solution {
-
+    public int addDigits(int num) {
+        while(num/10!=0){
+            int sum =0;
+            while(num!=0){
+                sum += num%10;
+                num/=10;
+            }
+            num = sum;
+        }
+        return num;
+    }
 }
