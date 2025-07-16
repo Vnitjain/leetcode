55080dc133a4b09d9bdf46bbac7900fd6fb6cb24
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 19:55:07 -0400

    resolved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,15 @@
 package my.leetcode;
 
 public class Solution {
+    public int countDigits(int num) {
+        int number = num;
 
+        int count = 0;
+        while(number!=0){
+            if(num%(number%10)==0)
+                count++;
+            number/=10;
+        }
+        return count;
+    }
 }
