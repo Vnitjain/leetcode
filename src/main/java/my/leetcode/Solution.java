fccb65e8a4b98fddb394ed2f6ad4a3795c06c2e7
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 19:47:29 -0400

    resolved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,21 @@
 package my.leetcode;
 
 public class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max = 0;
 
+        for(String currStr:sentences){
+            max = Math.max(max,countWords(currStr));
+        }
+        return max;
+    }
+
+    private int countWords(String str){
+        int count = 0;
+        for (char c:str.toCharArray()){
+            if(c==' ')
+                count++;
+        }
+        return  count+1;
+    }
 }
