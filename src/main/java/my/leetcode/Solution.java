ec800156bb809fa7936961040a0dd6819873a2e7
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 19:33:24 -0400

    solved 98%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,18 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+
+        int i=0;
+        while(i<nums.length){
+            int temp = nums[i];
+            nums[i] = nums[i+1];
+            nums[i+1] = temp;
+            i+=2;
+        }
+        return nums;
+    }
 }
