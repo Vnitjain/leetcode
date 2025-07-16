a7287e0821c7fd5bc8950e4f0f35234cf6260627
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 19:38:56 -0400

    resolved 84%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,18 @@
 package my.leetcode;
 
 public class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int[] words = new int[26];
 
+        for(char c:sentence.toCharArray()){
+            words[(int)c-97]++;
+        }
+
+        for (int i:words){
+            if(i==0)
+                return false;
+        }
+
+        return true;
+    }
 }
