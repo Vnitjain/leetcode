92ede9e04fdc11a373abac1cf1cf0de75c9436a5
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 20:02:54 -0400

    resolved 55%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,22 @@
 package my.leetcode;
 
 public class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String str:words){
+            if(isPalindrome(str))
+                return str;
+        }
+        return "";
+    }
 
+    private boolean isPalindrome(String str){
+        int i=0;
+        while(i<str.length()/2){
+            if(str.charAt(i)!=str.charAt(str.length()-1-i)){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
 }
