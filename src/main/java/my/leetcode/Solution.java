6acabac461ab894a40fd4311ccc30b66e90cb82e
Vinit Jain <jain.vinit14@gmail.com>
2025-07-16 20:05:31 -0400

    resolved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,13 @@
 package my.leetcode;
 
 public class Solution {
-
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for (int hour:hours) {
+            if(hour>=target){
+                count++;
+            }
+        }
+        return count;
+    }
 }
