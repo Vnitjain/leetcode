8292455ab6aa059a8b93359e67647d33a792ef7b
Vinit Jain <jain.vinit14@gmail.com>
2025-07-20 19:39:22 -0400

    solved 95%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package my.leetcode;
 
 public class Solution {
+    public boolean judgeCircle(String moves) {
+        int[] freq = new int[2];
 
+        for (char move : moves.toCharArray()) {
+            if (move == 'U')
+                freq[0]++;
+            else if (move == 'D')
+                freq[0]--;
+            else if (move == 'L')
+                freq[1]++;
+            else
+                freq[1]--;
+        }
+        return freq[0] == 0 && freq[1] == 0;
+    }
 }
