9d4f377866de2c5d7eab7d93b57e6c6ce6aebad4
Vinit Jain <jain.vinit14@gmail.com>
2025-07-20 14:13:27 -0400

    solved 99%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,25 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class Solution {
+    public List<String> removeSubfolders(String[] folder) {
+        Set<String> seenFolders = new HashSet<>(Arrays.asList(folder));
+        List<String> result = new ArrayList<>();
+
+        for(String path: folder){
+            result.add(path);
+            for(int i=0;i<path.length();i++){
+                if(path.charAt(i)=='/' && seenFolders.contains(path.substring(0,i)) ){
+                    result.removeLast();
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
