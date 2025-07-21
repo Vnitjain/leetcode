a4b9f08c3adfec8af9dbe61195e2a5c8ece458f7
Vinit Jain <jain.vinit14@gmail.com>
2025-07-21 10:08:10 -0400

    Revert "solved 70%"
    This reverts commit 78d3739a9d599bcd0c05a77030048722f5f50534.

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,21 +1,5 @@
 package my.leetcode;
 
-
 public class Solution {
-    public int partitionString(String s) {
-        if (s.length() < 2) {
-            return s.length();
-        }
 
-        int j = 0,count=0;
-        while (j < s.length()) {
-            int[] seen = new int[26];
-            while (j < s.length()&&seen[(int)s.charAt(j)-97]==0) {
-                seen[(int)s.charAt(j)-97]++;
-                j++;
-            }
-            count++;
-        }
-        return count;
-    }
 }
