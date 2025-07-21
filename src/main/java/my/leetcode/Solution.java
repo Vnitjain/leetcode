66d5f09db50a4fbb7d2be095c69e6e8d45a7caff
Vinit Jain <jain.vinit14@gmail.com>
2025-07-21 10:15:24 -0400

    solved 70%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,21 @@
 package my.leetcode;
 
-public class Solution {
 
+public class Solution {
+    public int partitionString(String s) {
+        if (s.length() < 2) {
+            return s.length();
+        }
+
+        int j = 0,count=0;
+        while (j < s.length()) {
+            int[] seen = new int[26];
+            while (j < s.length()&&seen[(int)s.charAt(j)-97]==0) {
+                seen[(int)s.charAt(j)-97]++;
+                j++;
+            }
+            count++;
+        }
+        return count;
+    }
 }
