5b43d0dfbd66f129ce9bce96724ce57e869dfc22
Vinit Jain <jain.vinit14@gmail.com>
2025-07-30 17:42:27 -0400

    solved 100%

--- go/leetcode.go
+++ go/leetcode.go
@@ -3,3 +3,13 @@ package main
 func main() {
 
 }
+
+func sumOfMultiples(n int) int {
+	sum := 0
+	for i := 1; i < n+1; i++ {
+		if i%3 == 0 || i%5 == 0 || i%7 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
