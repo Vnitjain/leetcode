33f765ae3d35ebcf2ce2c930dc5542363a078a58
Vinit Jain <jain.vinit14@gmail.com>
2025-07-30 17:32:56 -0400

    solved 100%

--- go/leetcode.go
+++ go/leetcode.go
@@ -3,3 +3,15 @@ package main
 func main() {
 
 }
+
+func differenceOfSum(nums []int) int {
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+		for nums[i] != 0 {
+			sum -= nums[i] % 10
+			nums[i] /= 10
+		}
+	}
+	return sum
+}
