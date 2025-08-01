71dfe5a9a1a7a1cc9fa4f216de934f5c385d3311
Vinit Jain <jain.vinit14@gmail.com>
2025-08-01 17:27:52 -0400

    solved 8%

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function triangleType(nums) {
+    nums.sort((a, b) => b - a);
+    if (nums[0] >= nums[1] + nums[2])
+        return "none"
+    let seen = new Set();
+    for (let i = 0; i < 3; i++)
+        seen.add(nums[i]);
+    return seen.size == 3 ? "scalene" : seen.size == 2 ? "isosceles" : seen.size == 1 ? "equilateral" : "none";
+}
