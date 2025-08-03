8242811b46963f843c03f036e1111732f2b40c06
Vinit Jain <jain.vinit14@gmail.com>
2025-08-02 21:35:11 -0400

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,15 @@
 package my.leetcode;
 
 public class Solution {
-
+    public void reverseString(char[] s) {
+        int length = s.length;
+        for (int i = 0; i < length /2; i++) {
+            int right = length - 1 - i;
+            if(s[i]!=s[right]){
+                char temp = s[i];
+                s[i] = s[right];
+                s[right] = temp;
+            }
+        }
+    }
 }
