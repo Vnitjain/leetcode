9979ca901a0768a2b7d20fd9fefde9527fba241f
Vinit Jain <jain.vinit14@gmail.com>
2025-08-02 21:21:51 -0400

    solved 60%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,33 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.List;
+import java.util.Optional;
 
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+ }
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return traverseTree(root,p,q);
+    }
+    private TreeNode traverseTree(TreeNode currNode, TreeNode p,TreeNode q){
+        if(currNode==null)
+            return currNode;
+        if(currNode.val==p.val || currNode.val==q.val)
+            return currNode;
+        TreeNode leftNode = traverseTree(currNode.left,p,q);
+        TreeNode rightNode = traverseTree(currNode.right,p,q);
+        if(leftNode!=null&&rightNode!=null){
+            return currNode;
+        } else if (leftNode!=null) {
+            return leftNode;
+        }else if (rightNode!=null){
+            return rightNode;
+        } else return null;
+    }
 }
+
