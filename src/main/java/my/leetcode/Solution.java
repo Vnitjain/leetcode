bbbc15d47f1eb17c0d54353ed5e3785ce2e4a69d
Vinit Jain <jain.vinit14@gmail.com>
2025-08-06 11:00:20 -0400

    solved 30%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,51 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    Map<Integer, Integer> freq;
+    int max;
+
+    public int[] findMode(TreeNode root) {
+        freq = new HashMap<>();
+        max = Integer.MIN_VALUE;
+        traverseTree(root);
+        List<Integer> result = new ArrayList<>();
+        for (int key : freq.keySet()) {
+            if (freq.get(key) == max)
+                result.add(key);
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private void traverseTree(TreeNode curr) {
+        if (curr == null)
+            return;
+        freq.put(curr.val, freq.getOrDefault(curr.val, 0) + 1);
+        max = freq.get(curr.val) > max ? freq.get(curr.val) : max;
+        traverseTree(curr.left);
+        traverseTree(curr.right);
+    }
 }
