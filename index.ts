dcf9c8d8a089a57129331fc98d965f9889096ed4
Vinit jain <jain.vinit14@gmail.com>
2025-09-16 23:28:33 -0400

    solved 40%

--- index.ts
+++ index.ts
@@ -1,15 +1,13 @@
 function longestConsecutive(nums: number[]): number {
     if (nums.length === 0) return 0;
-    nums = Array.from(new Set<number>(nums)).sort((a, b) => a - b);
+    let set = new Set(nums);
     let final = 1;
-    let i = 0, j = i + 1;
-    while (j < nums.length) {
-        while (nums[j] - 1 === nums[j - 1]) {
-            j++;
+    for (const num of set)
+        if (!set.has(num - 1)) {
+            let i = 1;
+            while (set.has(num + i))
+                i++;
+            final = Math.max(final, i);
         }
-        final = Math.max(final, j - i)
-        i = j;
-        j = i + 1;
-    }
     return final;
 };
