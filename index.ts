9a37c80d8142dcbce714ffdd2641ad2d047a91bd
Vinit Jain <jain.vinit14@gmail.com>
2025-09-23 19:37:45 -0400

    solved 25%

--- index.ts
+++ index.ts
@@ -1,33 +1,33 @@
-class MinStack {
-    private stack: number[];
-    private minStack: number[];
-    constructor() {
-        this.stack = [];
-        this.minStack = [];
-    }
+    class MinStack {
+        private stack: number[];
+        private minStack: number[];
+        constructor() {
+            this.stack = [];
+            this.minStack = [];
+        }
 
-    push(val: number): void {
-        this.stack.push(val);
-        if (val < this.topMinStack())
-            this.minStack.push(val);
-    }
+        push(val: number): void {
+            this.stack.push(val);
+            if (val <= this.topMinStack())
+                this.minStack.push(val);
+        }
 
-    pop(): void {
-        const top = this.top();
-        this.stack = this.stack.slice(0, this.stack.length - 1)
-        if (top === this.topMinStack())
-            this.minStack = this.minStack.slice(0, this.minStack.length - 1)
-    }
+        pop(): void {
+            const top = this.top();
+            this.stack = this.stack.slice(0, this.stack.length - 1)
+            if (top === this.topMinStack())
+                this.minStack = this.minStack.slice(0, this.minStack.length - 1)
+        }
 
-    top(): number {
-        return this.stack[this.stack.length - 1]
-    }
+        top(): number {
+            return this.stack[this.stack.length - 1]
+        }
 
-    topMinStack(): number {
-        return this.minStack[this.minStack.length - 1] ?? Number.MAX_SAFE_INTEGER;
-    }
+        topMinStack(): number {
+            return this.minStack[this.minStack.length - 1] ?? Number.MAX_SAFE_INTEGER;
+        }
 
-    getMin(): number {
-        return this.topMinStack();
+        getMin(): number {
+            return this.topMinStack();
+        }
     }
-}
