b67b6bf10ab30b7ee1719113d2d4bd02af7c3ce6
Vinit Jain <jain.vinit14@gmail.com>
2025-09-28 19:23:28 -0400

    solved 100%

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 function generateParenthesis(n: number): string[] {
     let finalParenthesis: string[] = [];
-    parenthesis("(", 1, 0, ["("], finalParenthesis, n);
+    parenthesis("(", 1, 0, finalParenthesis, n);
     return finalParenthesis
 };
 
-function parenthesis(currString: string, leftCount: number, rightCount: number, stack: string[], finalParenthesis: string[], n: number): void {
-    if (leftCount <= n && rightCount <= n) {
-        if (stack.length === 0 && currString.length === n * 2)
-            finalParenthesis.push(currString);
-        parenthesis(currString + "(", leftCount + 1, rightCount, [...stack, "("], finalParenthesis, n)
-        stack.pop()
-        parenthesis(currString + ")", leftCount, rightCount + 1, [...stack], finalParenthesis, n)
-    }
+function parenthesis(currString: string, leftCount: number, rightCount: number, finalParenthesis: string[], n: number): void {
+    if (currString.length === n * 2)
+        finalParenthesis.push(currString);
+    if (leftCount < n)
+        parenthesis(currString + "(", leftCount + 1, rightCount, finalParenthesis, n)
+    if (rightCount < leftCount)
+        parenthesis(currString + ")", leftCount, rightCount + 1, finalParenthesis, n)
 }
 
 generateParenthesis(3)
