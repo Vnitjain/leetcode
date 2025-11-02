3926acf7ef1ab2a58d465756ae71a9dd81672f69
Vinit Jain <jain.vinit14@gmail.com>
2025-11-02 12:08:39 -0500

    solved 100%

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,37 @@
-describe("Test cases", () => {
+import { convertToCoords, searchMatrix } from ".";
+
+describe("searchMatrix function", () => {
   test("example 1", () => {
-    expect(1).toBe(1);
+    expect(
+      searchMatrix(
+        [
+          [1, 3, 5, 7],
+          [10, 11, 16, 20],
+          [23, 30, 34, 60],
+        ],
+        3,
+      ),
+    ).toBe(true);
+  });
+  test("example 2", () => {
+    expect(
+      searchMatrix(
+        [
+          [1, 3, 5, 7],
+          [10, 11, 16, 20],
+          [23, 30, 34, 60],
+        ],
+        13,
+      ),
+    ).toBe(false);
+  });
+});
+
+describe("convertToCoords Function", () => {
+  test("example 1", () => {
+    expect(convertToCoords(5, 3, 4)).toStrictEqual({ x: 1, y: 1 });
+  });
+  test("example 2", () => {
+    expect(convertToCoords(11, 3, 4)).toStrictEqual({ x: 2, y: 3 });
   });
 });
--- index.ts
+++ index.ts
@@ -1,5 +1,28 @@
-function searchMatrix(matrix: number[][], target: number): boolean {
+export function searchMatrix(matrix: number[][], target: number): boolean {
+  let rows = matrix.length,
+    columns = matrix[0].length;
+  let length = rows * columns;
+
   let i = 0,
-    j = matrix.length;
-  while (i < j) {}
+    j = length - 1;
+  while (i <= j) {
+    const mid = Math.floor((j - i) / 2) + i;
+    let coords = convertToCoords(mid, rows, columns);
+    if (matrix[coords.x][coords.y] === target) {
+      return true;
+    } else if (matrix[coords.x][coords.y] < target) {
+      i = mid + 1;
+    } else {
+      j = mid - 1;
+    }
+  }
+  return false;
+}
+
+export function convertToCoords(
+  index: number,
+  rows: number,
+  columns: number,
+): { x: number; y: number } {
+  return { x: Math.floor(index / columns), y: index % columns };
 }
