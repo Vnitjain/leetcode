e38f79fcba3a6f85efd41874677d172cfadf2403
Vinit Jain <jain.vinit14@gmail.com>
2025-11-11 13:52:12 -0500

    solved 25%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,9 @@
 package my.leetcode;
 
 public class Solution {
-
+    public int fib(int n) {
+        if (n == 0 || n == 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
 }
