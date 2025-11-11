f79b9d3b523999fd177a98041ed5d72139bac453
Vinit Jain <jain.vinit14@gmail.com>
2025-11-11 14:05:27 -0500

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,9 +1,21 @@
 package my.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+    Map<Integer, Integer> mem = new HashMap<>();
+
     public int fib(int n) {
-        if (n == 0 || n == 1)
-            return n;
-        return fib(n - 1) + fib(n - 2);
+        mem.put(0, 0);
+        mem.put(1, 1);
+        return fibonacci(n);
+    }
+
+    private int fibonacci(int n) {
+        if (mem.containsKey(n))
+            return mem.get(n);
+        mem.put(n, fib(n - 1) + fib(n - 2));
+        return mem.get(n);
     }
 }
