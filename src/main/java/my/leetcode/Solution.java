d87df0fa3bedab6865864dcad133a1dff3efc263
Vinit Jain <jain.vinit14@gmail.com>
2025-11-12 00:01:51 -0500

    solved 20%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -5,9 +5,13 @@ import java.util.Arrays;
 public class Solution {
     public int minimumDifference(int[] nums, int k) {
         Arrays.sort(nums);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length - k + 1; i++) {
-            min = Math.min(min, nums[i + k - 1] - nums[i]);
+        int min = Integer.MAX_VALUE, numL = nums.length;
+        if (numL <= k)
+            return nums[numL - 1] - nums[0];
+
+        for (int i = 0; i < numL - k + 1; i++) {
+            int newDiff = nums[i + k - 1] - nums[i];
+            min = Math.min(min, newDiff);
         }
         return min;
     }
