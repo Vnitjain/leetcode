59631d14de01bf367dce07a0954bca0e7eb72235
Vinit Jain <jain.vinit14@gmail.com>
2025-11-11 23:47:08 -0500

    solved 20%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,14 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length - k + 1; i++) {
+            min = Math.min(min, nums[i + k - 1] - nums[i]);
+        }
+        return min;
+    }
 }
