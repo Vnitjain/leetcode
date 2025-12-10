8d22d3aab14424ac35e467bc45b1990eea75b904
Vinit Jain <jain.vinit14@gmail.com>
2025-12-10 18:07:54 -0500

    solved 100%

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,2 +1,18 @@
 fn main() {
+    let s = Solution::to_lower_case(String::from("HELLO"));
+    print!("{}",s);
+}
+
+struct Solution{
+
+}
+
+impl Solution {
+    pub fn to_lower_case(s: String) -> String {
+        let mut result = String::new(); 
+        for characters in s.chars(){
+            result.push(characters.to_ascii_lowercase());
+        }
+        return s
+    }
 }
