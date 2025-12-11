e1b14f02e4939de702b42a9e542c5433d3a82971
Vinit Jain <jain.vinit14@gmail.com>
2025-12-10 23:27:55 -0500

    solved 100%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,13 @@
 package my.leetcode;
 
 public class Solution {
-
+    public int countKeyChanges(String s) {
+        int finalCount = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if ((s.charAt(i) != s.charAt(i + 1) && (Math.abs(s.charAt(i) - s.charAt(i + 1)) != 32))) {
+                finalCount++;
+            }
+        }
+        return finalCount;
+    }
 }
