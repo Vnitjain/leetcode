e97aaaa4d813ff690cfb18af8b05715b1384e9cf
Vinit Jain <jain.vinit14@gmail.com>
2025-12-13 01:02:52 -0500

    solved 64%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -4,21 +4,11 @@ import java.util.Arrays;
 
 public class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
-        int[] prefix = new int[nums.length + 1];
         Arrays.sort(nums);
-        for (int i = 1; i < prefix.length; i++)
-            prefix[i] = prefix[i - 1] + nums[i - 1];
-
-        for (int i = 0; i < queries.length; i++) {
-            int j = 0;
-            while (j < prefix.length) {
-                if (prefix[j] <= queries[i])
-                    j++;
-                else
-                    break;
-            }
-            queries[i] = j - 1;
-        }
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+        for (int i = 0; i < queries.length; i++)
+            queries[i] = (Math.abs(Arrays.binarySearch(nums, queries[i]) + 1));
         return queries;
     }
 }
