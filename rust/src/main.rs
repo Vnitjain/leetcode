e93da7f63d8f1c800c186816d4daa1d0efbc1f8d
Vinit Jain <jain.vinit14@gmail.com>
2025-12-13 01:42:20 -0500

    solved 100%

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,2 +1,27 @@
 fn main() {
 }
+
+
+struct Solution{
+
+}
+
+impl Solution {
+    pub fn answer_queries(nums: Vec<i32>, queries: Vec<i32>) -> Vec<i32> {
+        let mut n = nums;
+        n.sort();
+        for i in 1..n.len(){
+            n[i] += n[i-1];
+        }
+        print!("{:?}",n);
+        let mut q = queries;
+        for i in 0..q.len(){
+            match n.binary_search(&q[i]) {
+                Ok(index) => 
+                    q[i] = (index+1) as i32,
+                Err(index) => q[i]=index as i32,
+            }
+        }
+        return q;
+    }
+}
