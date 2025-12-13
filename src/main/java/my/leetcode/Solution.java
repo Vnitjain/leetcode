d1436a17eca48f238fde792acd6290faaeab0c44
Vinit Jain <jain.vinit14@gmail.com>
2025-12-12 22:41:39 -0500

    solved 18%

--- src/main/java/my/leetcode/Solution.java
+++ src/main/java/my/leetcode/Solution.java
@@ -1,5 +1,26 @@
 package my.leetcode;
 
-public class Solution {
+import java.util.Arrays;
 
+public class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int[] solution = new int[queries.length];
+        Arrays.sort(nums);
+
+        System.out.println(Arrays.toString(nums));
+
+        for (int i = 0; i < queries.length; i++) {
+            int sum = 0;
+            int j = 0;
+            while (j < nums.length) {
+                if (sum + nums[j] <= queries[i])
+                    sum += nums[j++];
+                else
+                    break;
+            }
+
+            solution[i] = j;
+        }
+        return solution;
+    }
 }
--- src/test/java/my/leetcode/AppTest.java
+++ src/test/java/my/leetcode/AppTest.java
@@ -8,7 +8,7 @@ public class AppTest {
 
     @Test
     public void test() {
-
+        s.answerQueries(new int[] { 4, 5, 2, 1 }, new int[] { 3, 10, 21 });
     }
 
     private void startTimer() {
