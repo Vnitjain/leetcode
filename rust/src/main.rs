41203f18c089d48f26aa021a6e8906ca34952a68
Vinit Jain <jain.vinit14@gmail.com>
2025-12-13 22:28:43 -0500

    solved 100%

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,2 +1,35 @@
+use core::num;
+
 fn main() {
+    let i1 = String::from("2080-02-29");
+    let o1 = String::from("100000100000-10-11101");
+    let t1 = Solution::convert_date_to_binary(i1);
+    print!("{}",t1);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn convert_date_to_binary(date: String) -> String {
+        let mut solString = "".to_string();
+        let dateSplit = date.split("-");
+        for (i,num) in dateSplit.enumerate() {
+            let mut curr = match num.parse::<i32>() {
+                Ok(value) => value,
+                Err(_) => 0,
+            };
+
+            let mut currBin = String::new();
+            if i!=0{
+                solString.push('-');
+            }
+            while curr != 0 {
+                let bit = if curr % 2 == 0 { '0' } else { '1' };
+                currBin = bit.to_string() + &currBin;
+                curr = &curr / 2;
+            }
+            solString.push_str(&currBin);
+        }
+        return solString;
+    }
 }
